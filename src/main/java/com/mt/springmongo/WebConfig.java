8547d8fdb5fe9716dca0701c3eb6e9ede3bc295a
asad059 <115538977+asad059@users.noreply.github.com>
2023-11-06 13:21:15 +0000

    Update WebConfig.java

--- src/main/java/com/mt/springmongo/WebConfig.java
+++ src/main/java/com/mt/springmongo/WebConfig.java
@@ -3,7 +3,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+ 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
